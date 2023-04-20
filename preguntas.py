b0f4a15104fc99ad604cf6ba4efe7f56521fab49
dramirezla <53583408+dramirezla@users.noreply.github.com>
2023-04-19 20:36:26 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -83,7 +83,6 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    ""
     return tbl0.groupby('_c1')['_c2'].max()
 
 
@@ -154,4 +153,123 @@ def pregunta_09():
     return tbl0
 
 
+def pregunta_10():
+    """
+    Construya una tabla que contenga _c1 y una lista separada por ':' de los valores de
+    la columna _c2 para el archivo `tbl0.tsv`.
+
+    Rta/
+                                   _c1
+      _c0
+    0   A              1:1:2:3:6:7:8:9
+    1   B                1:3:4:5:6:8:9
+    2   C                    0:5:6:7:9
+    3   D                  1:2:3:5:5:7
+    4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
+    """
+    
+    grouped = tbl0.groupby("_c1")["_c2"].apply(lambda x: ":".join(x.astype(str))).reset_index()
+    
+    lista_valores = grouped['_c2'].str.split(':')
+
+    lista_valores = lista_valores.map(lambda x: list(map(int, x)))
+
+    lista_valores = lista_valores.map(lambda x: sorted(x))
+
+    columna_ordenada = lista_valores.map(lambda x: ':'.join(map(str, x)))
+
+    grouped["_c2"]=columna_ordenada
+    grouped.rename(columns={"_c1": "_c0", "_c2": "_c1"}, inplace=True)
+    
+
+    return grouped
+
+def pregunta_11():
+    """
+    Construya una tabla que contenga _c0 y una lista separada por ',' de los valores de
+    la columna _c4 del archivo `tbl1.tsv`.
+
+    Rta/
+        _c0      _c4
+    0     0    b,f,g
+    1     1    a,c,f
+    2     2  a,c,e,f
+    3     3      a,b
+    ...
+    37   37  a,c,e,f
+    38   38      d,e
+    39   39    a,d,f
+    """
+    
+    grouped = tbl1.groupby("_c0")["_c4"].apply(lambda x: ",".join(x)).reset_index()
+    lista_valores = grouped['_c4'].str.split(',')
+
+    lista_valores = lista_valores.map(lambda x: list(x))
+
+    lista_valores = lista_valores.map(lambda x: sorted(x))
+
+    columna_ordenada = lista_valores.map(lambda x: ','.join(map(str, x)))
+
+    grouped["_c4"]=columna_ordenada
+
+    
+    return grouped
+
+
+def pregunta_12():
+    """
+    Construya una tabla que contenga _c0 y una lista separada por ',' de los valores de
+    la columna _c5a y _c5b (unidos por ':') de la tabla `tbl2.tsv`.
+
+    Rta/
+        _c0                                  _c5
+    0     0        bbb:0,ddd:9,ggg:8,hhh:2,jjj:3
+    1     1              aaa:3,ccc:2,ddd:0,hhh:9
+    2     2              ccc:6,ddd:2,ggg:5,jjj:1
+    ...
+    37   37                    eee:0,fff:2,hhh:6
+    38   38                    eee:0,fff:9,iii:2
+    39   39                    ggg:3,hhh:8,jjj:5
+    """
+
+
+    
+    tbl2['_c5'] = tbl2['_c5a'] + ':' + tbl2['_c5b'].astype(str)
+
+    
+    tabla = tbl2.groupby('_c0')['_c5'].apply(lambda x: ','.join(x)).reset_index()
+    
+    lista_valores = tabla['_c5'].str.split(',')
+
+    lista_valores = lista_valores.map(lambda x: list(x))
+
+    lista_valores = lista_valores.map(lambda x: sorted(x))
+
+    columna_ordenada = lista_valores.map(lambda x: ','.join(map(str, x)))
+
+    tabla["_c5"]=columna_ordenada
+
+    return tabla
+
+
+
+def pregunta_13():
+    """
+    Si la columna _c0 es la clave en los archivos `tbl0.tsv` y `tbl2.tsv`, compute la
+    suma de tbl2._c5b por cada valor en tbl0._c1.
+
+    Rta/
+    _c1
+    A    146
+    B    134
+    C     81
+    D    112
+    E    275
+    Name: _c5b, dtype: int64
+    """
+    merged = pd.merge(tbl0, tbl2, on='_c0')
+
+    result = merged.groupby('_c1')['_c5b'].sum()
+    
+    return result
 
